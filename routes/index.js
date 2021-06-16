1d059de08572a2f027f5fbb71aec34952a346a66
Fariha Rajput <fareeharana321@gmail.com>
2021-06-16 18:05:10 +0500

    Update index.js file

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-var express = require('express');
+var express = require("express");
+const UserControllers = require("../Controllers/UserControllers");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 router.get("/GetAllUsers", UserController.GetAllUsers);
 router.get("/SignupUser/:UserName&:UserPassword", UserController.SignUpUser);
 router.get("/LoginUser/:UserName&:UserPassword", UserController.LoginUser);
-
 
 module.exports = router;
